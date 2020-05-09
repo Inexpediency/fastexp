16e01586376ec95eeb69f3625bf5374f37947e2a
Ythosa <vasus714@yandex.ru>
2020-05-10 00:30:18 +0300

    theme generator added

--- apiworker/themes/theme.js
+++ apiworker/themes/theme.js
@@ -1,9 +1,9 @@
 const server_url = require('../server_url')
 
 class Theme {
-    static get_theme_url = '/private/get_theme'
+    static get_themes_url = '/private/get_themes'
 
-    static async get_theme(technology_id) {
+    static async get_themes(technology_id) {
         let myHeaders = new Headers();
         myHeaders.append('Content-Type', 'application/json');
         
@@ -17,28 +17,24 @@ class Theme {
             body: raw,
         };
 
-        let theme = {
+        let themes = {
             'err': null,
-            'title': null,
-            'content': null,
-            'id': null,
+            'themes': null
         };
 
         try {
-            let res = await fetch(server_url + Theme.get_theme_url, requestOptions)
+            let res = await fetch(server_url + Theme.get_themes_url, requestOptions)
             if (res.ok) {
                 res = await res.json()
-                theme.title = res.title
-                theme.content = res.content
-                theme.id = res.id
+                themes.themes = res.themes
             } else {
-                theme.err = true
+                themes.err = true
             }
         } catch(err) {
-            theme.err = err
+            themes.err = err
         }
         
-        return theme
+        return themes
     }
 }
 
