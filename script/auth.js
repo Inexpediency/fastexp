1e70229dcf10fb5b6f69207fd2a9e42873564633
Vadim Sabirov <vadimsabirov228@gmail.com>
2020-05-10 00:32:16 +0500

    rework mainPage

--- script/auth.js
+++ script/auth.js
@@ -1,5 +1,19 @@
-let email = document.querySelector('#email');
-let pass = document.querySelector('#pass');
-let btn = document.querySelector('#submit');
+let email = document.querySelector("#email");
+let pass = document.querySelector("#pass");
+let btn = document.querySelector("#submit");
 
-btn.onclick = () 
+btn.onclick = () => {
+  let authRequest = new Promise((resolve, reject) => {
+    fetch("localhost:8080/sessions", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body:
+        '{"email": "' + email.value + '", "password": "' + pass.value + '"}',
+    }).then((data) => resolve(data.json()));
+  });
+  authRequest.then(() => {
+    if (status === 200) {
+      window.location = "technologyes.html";
+    }
+  });
+};
