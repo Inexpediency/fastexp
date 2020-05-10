9d5d84eb202c40f1857ca0808f9f84e8b6db6780
Vadim Sabirov <vadimsabirov228@gmail.com>
2020-05-10 08:59:29 +0500

    Адаптивность шапки сайта и themes.html

--- script/profileTag.js
+++ script/profileTag.js
@@ -6,12 +6,11 @@ Users.whoami().then((result) => {
     h1.textContent = "С возвращением, " + result.email;
     for (let i = 0; i < tags.children.length; i++) {
       for (let k = 0; k < result.tags.length; k++)
-        if(tags.children[i].textContent === result.tags[k]) {
-            let img = document.createElement('img');
-            img.setAttribute('src', 'image/profile.jpg');
-            tags.children[i].appendChild(img);
+        if (tags.children[i].textContent === result.tags[k]) {
+          let img = document.createElement("img");
+          img.setAttribute("src", "image/profile.jpg");
+          tags.children[i].appendChild(img);
         }
-      
     }
   } else {
     window.location = "auth.html";
